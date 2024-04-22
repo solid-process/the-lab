6c88ee43d48b293f1ab37e4ef7a2edbb6b71c24b
Diego Linhares <diegomlinhares@gmail.com>
2024-04-22 19:36:23 -0300

    feat: Create ::Solid::Process::EventLogsRecord asynchronously using threads

--- lib/solid/process/event_logs_record.rb
+++ lib/solid/process/event_logs_record.rb
@@ -8,13 +8,23 @@ class Solid::Process::EventLogsRecord < ActiveRecord::Base
     include ::Solid::Result::EventLogs::Listener
 
     def on_finish(event_logs:)
+      ::Thread.new do
+        ::ActiveRecord::Base.connection_pool.with_connection do
+          create_event_logs(event_logs)
+        end
+      end
+    end
+
+    private
+
+    def create_event_logs(event_logs)
       root_name = event_logs.dig(:records, 0, :root, :name) || "Unknown"
       metadata = event_logs[:metadata]
       records = event_logs[:records].map do |record|
         record.deep_transform_values { _1.is_a?(::Solid::Process) ? _1.class.name : _1 }
       end
 
-      Solid::Process::EventLogsRecord.create!(
+      ::Solid::Process::EventLogsRecord.create!(
         version: event_logs[:version],
         root_name: root_name,
         trace_id: metadata[:trace_id],
@@ -22,6 +32,7 @@ class Solid::Process::EventLogsRecord < ActiveRecord::Base
         ids: metadata[:ids],
         records: records
       )
+
     rescue => e
       err = "#{e.message} (#{e.class}); Backtrace: #{e.backtrace.join(", ")}"
 
