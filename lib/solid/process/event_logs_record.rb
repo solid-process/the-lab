93e93ec00c3391a7439b3442d22638611a862488
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2024-04-23 00:39:58 -0300

    Add Solid::Process::EventLogsRecord#main_records

--- lib/solid/process/event_logs_record.rb
+++ lib/solid/process/event_logs_record.rb
@@ -12,8 +12,22 @@ class Solid::Process::EventLogsRecord < ActiveRecord::Base
     self[:records]
   end
 
+  INTERMEDIATE_TYPES = %w[_continue_ _given_].freeze
+
+  def raw_main_records
+    raw_records.filter_map do
+      type = _1.dig("result", "type")
+
+      _1 if (type == "_given_" && _1.dig("current", "id") == 0) || !type.in?(INTERMEDIATE_TYPES)
+    end
+  end
+
   def records
     Records.deserialize(raw_records)
+  end
+
+  def main_records
+    Records.deserialize(raw_main_records)
   end
 end
 # rubocop:enable Rails/ApplicationRecord
