c827d5d0961ae0b562ff299e03e2996872d65214
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2024-04-25 10:05:02 -0300

    Add EventLogs::JsonLoggerListener

--- config/initializers/solid_process.rb
+++ config/initializers/solid_process.rb
@@ -6,13 +6,14 @@ require "solid/validators/email_validator"
 require "solid/validators/instance_of_validator"
 
 require "solid/process/event_logs/record"
-require "solid/process/event_logs/basic_logger_listener"
+require "solid/process/event_logs/json_logger_listener"
+# require "solid/process/event_logs/basic_logger_listener"
 
-Solid::Process::EventLogs::BasicLoggerListener.logger = Rails.logger
+# Solid::Process::EventLogs::BasicLoggerListener.logger = Rails.logger
 
 Solid::Result.configuration do |config|
   config.event_logs.listener = Solid::Result::EventLogs::Listeners[
-    Solid::Process::EventLogs::BasicLoggerListener,
+    Solid::Process::EventLogs::JsonLoggerListener,
     Solid::Process::EventLogs::Record::Listener
   ]
 end
