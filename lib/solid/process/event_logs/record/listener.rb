0128863e28745a77618130a0d56c0b3236b4c372
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2024-04-25 10:04:30 -0300

    Replace json storage with sp serialization

--- lib/solid/process/event_logs/record/listener.rb
+++ lib/solid/process/event_logs/record/listener.rb
@@ -21,19 +21,10 @@ module Solid::Process::EventLogs
     end
 
     def create_record!(event_logs)
-      root_name = event_logs.dig(:records, 0, :root, :name) || "Unknown"
-      metadata = event_logs[:metadata]
-      records = JsonStorage::Records.serialize(event_logs[:records])
-
       ::Rails.error.record do
-        Record.create!(
-          version: event_logs[:version],
-          root_name: root_name,
-          trace_id: metadata[:trace_id],
-          duration: metadata[:duration],
-          ids: metadata[:ids],
-          records: records
-        )
+        serializad_event_logs = Serialization::Model.serialize(event_logs)
+
+        Record.create!(serializad_event_logs.attributes)
       end
     end
   end
