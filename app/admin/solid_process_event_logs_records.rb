5d2d96696026f411c1c4b3e287a816e3b4dc1501
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2024-04-25 22:34:10 -0300

    Improve app/admin/solid_process_event_logs_records.rb

--- app/admin/solid_process_event_logs_records.rb
+++ app/admin/solid_process_event_logs_records.rb
@@ -3,48 +3,48 @@
 
   permit_params :root_name, :trace_id, :version, :duration, :ids, :records
 
-  actions :all, except: []
-
-  filter :id
   filter :root_name
-  filter :trace_id
-  filter :duration
   filter :created_at
+  filter :duration
+  filter :trace_id
+  filter :id
 
   index do
     selectable_column
     id_column
     column :root_name
-    column :trace_id
-    column :duration
     column :created_at
+    column :duration
+    column :trace_id
     actions
   end
 
   show do
-    attributes_table_for(resource) do
-      row :id
-      row :root_name
-      row :trace_id
-      row :version
-      row :duration
-      row :ids
-      row :records
-      row :created_at
-      row :updated_at
+    panel "Event Log" do
+      attributes_table_for(resource) do
+        row :root_name
+        row :created_at
+        row :duration
+        row :trace_id
+      end
     end
-  end
 
-  form do |f|
-    f.semantic_errors(*f.object.errors.attribute_names)
-    f.inputs do
-      f.input :root_name
-      f.input :trace_id
-      f.input :version
-      f.input :duration
-      f.input :ids
-      f.input :records
+    panel "Records" do
+      attributes_table_for(resource) do
+        row "Serialized" do |record|
+          rows = record.raw_records.map.with_index do |raw_record, index|
+            "<tr><th>#{index}</th><td>#{raw_record.to_json}</td></tr>"
+          end
+
+          "<table>#{rows.join("\n")}</table>".html_safe
+        end
+        row :ids do |record|
+          rows = record.ids.map { |key, value| "<tr><th>#{key}</th><td>#{value.to_json}</td></tr>" }
+
+          "<table>#{rows.join("\n")}</table>".html_safe
+        end
+        row :version
+      end
     end
-    f.actions
   end
 end
